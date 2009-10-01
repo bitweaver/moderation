05b0f40a3047e6ebc27f7b8ffeebf7c9420b4910
wjames5 <will@tekimaki.com>
2009-10-01 14:17:02 +0000

    remove ref to non-existant copyright.txt file

--- ModerationSystem.php
+++ ModerationSystem.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_moderation/ModerationSystem.php,v 1.24 2009/10/01 13:45:44 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_moderation/ModerationSystem.php,v 1.25 2009/10/01 14:17:02 wjames5 Exp $
  *
  * +----------------------------------------------------------------------+
  * | Copyright ( c ) 2008, bitweaver.org
  * +----------------------------------------------------------------------+
- * | All Rights Reserved. See copyright.txt for details and a complete
+ * | All Rights Reserved. See below for details and a complete
  * | list of authors.
  * | Licensed under the GNU LESSER GENERAL PUBLIC LICENSE.
  * | See http://www.gnu.org/copyleft/lesser.html for details
@@ -23,7 +23,7 @@
  * can use to register things for moderation and
  *
  * @author   nick <nick@sluggardy.net>
- * @version  $Revision: 1.24 $
+ * @version  $Revision: 1.25 $
  * @package  moderation
  */
 
--- admin/schema_inc.php
+++ admin/schema_inc.php
@@ -1,12 +1,12 @@
 <?php
 
 /**
- * @version $Header: /cvsroot/bitweaver/_bit_moderation/admin/schema_inc.php,v 1.8 2009/10/01 13:45:44 wjames5 Exp $
+ * @version $Header: /cvsroot/bitweaver/_bit_moderation/admin/schema_inc.php,v 1.9 2009/10/01 14:17:02 wjames5 Exp $
  *
  * +----------------------------------------------------------------------+
  * | Copyright ( c ) 2008, bitweaver.org
  * +----------------------------------------------------------------------+
- * | All Rights Reserved. See copyright.txt for details and a complete
+ * | All Rights Reserved. See below for details and a complete
  * | list of authors.
  * | Licensed under the GNU LESSER GENERAL PUBLIC LICENSE.
  * | See http://www.gnu.org/copyleft/lesser.html for details
@@ -22,7 +22,7 @@
  * This file contains the schema for the moderation package.
  *
  * @author   nick <nick@sluggardy.net>
- * @version  $Revision: 1.8 $
+ * @version  $Revision: 1.9 $
  * @package  moderation
  */
 
